d33a9e0b54ac434b66bf28a34aa89b486a3335f2
LittleHack3r <sxm6549@rit.edu>
2019-04-18 14:13:32 -0400

    testing files

---
+++ home/gas.py
@@ -0,0 +1,10 @@
+my dad told me that he’s paid to fill my moms
+
+
+ car up every single time since they
+started dating to now & at first i was all 
+that’s so cute blah blah but then it really hit me 
+.....this woman really has not paid for gas in almost
+
+
+# 20 years what a fuckin dream
